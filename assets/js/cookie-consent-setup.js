d58a23ddf6aba265e291c15099748476a9c5efb2
binderiya-bya <bb3174a@american.edu>
2026-02-12 21:56:56 +0000

    Deploying to gh-pages from @ binderiya-bya/binderiya-bya.github.io@b369a0f3c377e891117ec61ba816508fc37b5af7 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://binderiyabyambasuren.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://binderiya-bya.github.io/#contact">contact us</a>.'
               }
             ]
           }
--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "binderiya-bya/binderiyabyambasuren.github.io",
+    "data-repo": "binderiya-bya/binderiya-bya.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
