47f13a787d4f5f81c571c0194833b7a2c2f240a4
binderiya-bya <bb3174a@american.edu>
2026-02-12 21:46:35 +0000

    Deploying to gh-pages from @ binderiya-bya/binderiyabyambasuren.github.io@8ffbec082a319d046a806fde1f917f333658f22f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "binderiya-bya/binderiyabyambasuren",
+    "data-repo": "binderiya-bya/binderiyabyambasuren.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
